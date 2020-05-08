3af69cf52a722b906b0d092c441bd4c7f15d62c4
kmchord9 <kmchord9@gmail.com>
2020-05-08 14:33:22 +0900

    edit sh

--- compose/vuecli/yarn-install-start.sh
+++ compose/vuecli/yarn-install-start.sh
@@ -7,12 +7,12 @@ cd $DIR_VUE
 #webpack errorの対処
 #yarn add eslint --dev
 
-#if [ ! -d node_modules ]; then
-
-yarn install
-yarn run build
-yarn run start
-#else 
-#  echo "node_modules find"
-#  yarn start
-#fi
+if [ ! -d node_modules ]; then
+  yarn install
+  yarn run build
+  yarn run start
+else 
+  echo "node_modules find"
+  yarn run build
+  yarn start
+fi
